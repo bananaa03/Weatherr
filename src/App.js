60a1b3a4102900e4857a5d3db6f054aa4113f8b5
bananaa03 <21520643@gm.uit.edu.vn>
2024-05-06 12:27:20 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { routes } from "./routes";
-//import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+// import LogIn from "./pages/LogIn/LogIn";
+
+// const fakeAuth = {
+//   isAuthenticated: false, // Đã đăng nhập hay chưa
+//   authenticate(cb) {
+//     fakeAuth.isAuthenticated = true;
+//     setTimeout(cb, 100); // Fake async
+//   },
+//   signout(cb) {
+//     fakeAuth.isAuthenticated = false;
+//     setTimeout(cb, 100); // Fake async
+//   }
+// };
+
+// const PrivateRoute = ({ element, ...rest }) => {
+//   return fakeAuth.isAuthenticated ? (
+//     <Route {...rest} element={element} />
+//   ) : (
+//     <Navigate to="/login" />
+//   );
+// };
 
 function App() {
   return (
@@ -10,19 +30,16 @@ function App() {
         <Routes>
           {routes.map((route) => {
             const Page = route.page;
-            //const Layout = route.isShowHeader ? DefaultComponent : Fragment;
             return (
+              //<PrivateRoute
               <Route
                 key={route.path}
                 path={route.path}
-                element={
-                  //<Layout>
-                    <Page />
-                  //</Layout>
-                }
+                element={<Page />}
               />
             );  
           })}
+          {/* <Route path="/login" element={<LogIn />} /> */}
         </Routes>
       </Router>
     </div>
