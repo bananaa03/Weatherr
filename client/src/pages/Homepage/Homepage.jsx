ce825df3f58f3960cb05b8f8fa7b48679f63f4e5
bananaa03 <21520643@gm.uit.edu.vn>
2024-05-23 22:36:26 +0700

    commint

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Homepage.css";
 import Grid from "@mui/material/Grid";
 import General from '../../components/General/General'
 import Detail from '../../components/Detail/Detail'
+import { useNavigate } from "react-router-dom";
 
 function Homepage() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Kiểm tra xem token có tồn tại không
+    const token = localStorage.getItem("token");
+    if (!token) {
+      // Nếu không có token, chuyển hướng người dùng đến trang đăng nhập
+      navigate('/login');
+    } else {
+      // Nếu có token, đánh dấu người dùng đã đăng nhập
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div id="main">
+      {!isLoggedIn && <h1>Please log in to access this page.</h1>}
+      {isLoggedIn && 
       <Grid container spacing={0}>
         <Grid item xs={4.5}>
           <General />
@@ -15,6 +33,7 @@ function Homepage() {
           <Detail />
         </Grid>
       </Grid>
+      }
     </div>
   );
 }
